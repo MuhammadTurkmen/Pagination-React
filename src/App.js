6581c6bcdbc32976a09973e4baa5278a83dfe0e3
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-22 14:47:01 +0430

    init

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   useEffect(() => {
     if(loading)return
-    setFollowers(data[page])
+    setFollowers(data[page]) 
   }, [loading, page])
   
   const handlePage = (index) => {
