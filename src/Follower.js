80a4a87265379e5ce9dce30648f7ef9ac9a58e7c
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-22 08:07:00 +0430

    init

--- src/Follower.js
+++ src/Follower.js
@@ -4,7 +4,7 @@ const Follower = ({avatar_url, html_url, login}) => {
   return <article className='card'>
     <img src={avatar_url} alt={login} />
     <h4>{login}</h4>
-    <a href={html_url}></a>
+    <a href={html_url} className='btn'>view profile</a>
   </article>
 }
 
