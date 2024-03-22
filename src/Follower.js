9333916089372da08a5de5da6fb0ee1a539c7b05
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-22 08:01:55 +0430

    init

--- src/Follower.js
+++ src/Follower.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Follower = ({avatar_url, html_url, login}) => {
   return <article className='card'>
-    
+    <img src={avatar_url} alt={login} />
+    <h4>{login}</h4>
   </article>
 }
 
