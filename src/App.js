5c20f14cfe3e9f296c4777162dac338f2f7bf2c4
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-22 12:56:46 +0430

    init

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useFetch } from './useFetch'
 import Follower from './Follower'
 function App() {
+  const {loading,data} = useFetch()
   return <h2>pagination starter</h2>
 }
 
