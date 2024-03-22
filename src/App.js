acc894c3800dac887b2609387948a07eddbcfc3c
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-22 14:13:15 +0430

    init

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useFetch } from './useFetch'
 import Follower from './Follower'
 function App() {
   const {loading,data} = useFetch()
-  
+  const [page, setPage] = useState(0)
   return <main>
     <div className="section-title">
       <h1>{loading ? 'loading...' : 'pagination'}</h1>
