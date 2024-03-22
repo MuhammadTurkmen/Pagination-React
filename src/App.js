1c97a06146676c42bea142e459a9b00dc4c2aaf4
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-22 14:44:27 +0430

    init

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ function App() {
     if(loading)return
     setFollowers(data[page])
   }, [loading])
+  
+  const handlePage = (index) => {
+    setPage(index)
+  }
 
 
   return <main>
@@ -27,7 +31,7 @@ function App() {
       
       {!loading && <div className="btn-container">
         {data.map((item, index) => {
-          return <button key={index} className='page-btn'>{index + 1}</button>
+          return <button key={index} className='page-btn' onClick={() => handlePage(index)}>{index + 1}</button>
         })}
       </div>}
     </section>
